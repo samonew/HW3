519f173c92fcaa4df1495cfe7554b1ecdfb4d1c4
samonewatkins <watkinss23@up.edu>
2020-02-09 15:40:08 -0800

    Second Commit- with buttons

--- src/main/java/edu/up/cs301/fangman/FangManActivity.java
+++ src/main/java/edu/up/cs301/fangman/FangManActivity.java
@@ -39,6 +39,34 @@ public class FangManActivity extends Activity {
         // load layout
         setContentView(R.layout.activity_fang_man);
 
+        //initialized all of the alphabet buttons
+        Button A = findViewById(R.id.button1);
+        Button B = findViewById(R.id.button2);
+        Button C = findViewById(R.id.button3);
+        Button D = findViewById(R.id.button4);
+        Button E = findViewById(R.id.button5);
+        Button F = findViewById(R.id.button6);
+        Button G = findViewById(R.id.button7);
+        Button H = findViewById(R.id.button8);
+        Button I = findViewById(R.id.button9);
+        Button J = findViewById(R.id.button10);
+        Button K = findViewById(R.id.button11);
+        Button L = findViewById(R.id.button12);
+        Button M = findViewById(R.id.button13);
+        Button N = findViewById(R.id.button14);
+        Button O = findViewById(R.id.button15);
+        Button P = findViewById(R.id.button16);
+        Button Q = findViewById(R.id.button17);
+        Button buttonR = findViewById(R.id.button18);
+        Button S = findViewById(R.id.button19);
+        Button T = findViewById(R.id.button20);
+        Button U = findViewById(R.id.button21);
+        Button V = findViewById(R.id.button22);
+        Button W = findViewById(R.id.button23);
+        Button X = findViewById(R.id.button24);
+        Button Y = findViewById(R.id.button25);
+        Button Z = findViewById(R.id.button26);
+
     }
 
 
